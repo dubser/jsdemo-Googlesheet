65f658b64d7a9bacadd4e2b7d61f2c46257fb72c
dubser <dubser123@hotmail.com>
2024-11-04 17:56:25 -0500

    Commit initial

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/*console.log(client_id );*/
+
+function test1() {
+ alert("test a été cliqué !");
+ console.log(Date() + "Test1 Running")
+ }
+function test2() {
+alert("test2 a été cliqué !");
+ }
+function test3() {
+alert("test3 a été cliqué !");
+ }
+function test4() {
+alert("test4 a été cliqué !");
+ }
